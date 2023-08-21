35fed9ad9c38a0cdf5ff04d9b2deb1f019978887
Gegel85 <Gegel85@laposte.net>
2023-08-21 16:59:29 +0200

    Updated intl header

--- src/SokuMod/integration.hpp
+++ src/SokuMod/integration.hpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 #include <locale>
 
+#ifndef TH123INTL_HPP
+#define TH123INTL_HPP
+
 namespace th123intl {
     inline unsigned int GetFileSystemCodePage() { return GetACP(); }
     inline unsigned int GetTextCodePage() {
@@ -11,52 +14,55 @@ namespace th123intl {
         return proc();
     }
 
-    template <_locale_t* curLocale>
+    template <_locale_t& curLocale>
     _locale_t GetLocale() {
-        if (*curLocale) return *curLocale;
+        if (curLocale) return curLocale;
         auto handle = GetModuleHandleA("th123intl.dll");
         FARPROC proc = 0;
         if (handle) proc = GetProcAddress(handle, "GetLocale");
         if (!handle || !proc) {
-            if (!*curLocale) *curLocale = _create_locale(LC_ALL, "ja_JP");
-            return *curLocale;
+            if (!curLocale) curLocale = _create_locale(LC_ALL, "ja_JP");
+            return curLocale;
         }
-        return *curLocale = (_locale_t)proc();
+        return curLocale = (_locale_t)proc();
     }
 
     template<int inFlags = MB_USEGLYPHCHARS, int outFlags = WC_COMPOSITECHECK|WC_DEFAULTCHAR>
     inline void ConvertCodePage(unsigned int fromCP, const std::string_view& from, unsigned int toCP, std::string& to) {
         if (fromCP == toCP) { to = from; return; }
         else {
+            DWORD dwFlags = fromCP == CP_UTF8 ? inFlags & WC_ERR_INVALID_CHARS : inFlags;
             std::wstring buffer;
-            size_t size = MultiByteToWideChar(fromCP, inFlags, from.data(), from.size(), NULL, NULL);
+            size_t size = MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), NULL, NULL);
             buffer.resize(size);
-            if (!MultiByteToWideChar(fromCP, inFlags, from.data(), from.size(), buffer.data(), buffer.size()))
+            if (!MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), buffer.data(), buffer.size()))
                 { to.resize(0); return; }
 
-            size = WideCharToMultiByte(toCP, outFlags, buffer.data(), -1, NULL, NULL, NULL, NULL);
+            dwFlags = toCP == CP_UTF8 ? outFlags & WC_ERR_INVALID_CHARS : outFlags;
+            size = WideCharToMultiByte(toCP, dwFlags, buffer.data(), buffer.size(), NULL, NULL, NULL, NULL);
             to.resize(size);
-            if (!WideCharToMultiByte(toCP, outFlags, buffer.data(), -1, to.data(), to.size(), NULL, NULL))
+            if (!WideCharToMultiByte(toCP, dwFlags, buffer.data(), buffer.size(), to.data(), to.size(), NULL, NULL))
                 { to.resize(0); return; }
-            to.resize(size-1);
         }
     }
 
     template<int flags = WC_COMPOSITECHECK|WC_DEFAULTCHAR>
     inline void ConvertCodePage(const std::wstring_view& from, unsigned int toCP, std::string& to) {
-        size_t size = WideCharToMultiByte(toCP, flags, from.data(), from.size(), NULL, NULL, NULL, NULL);
+        const DWORD dwFlags = toCP == CP_UTF8 ? flags & WC_ERR_INVALID_CHARS : flags;
+        size_t size = WideCharToMultiByte(toCP, dwFlags, from.data(), from.size(), NULL, NULL, NULL, NULL);
         to.resize(size);
-        if (!WideCharToMultiByte(toCP, flags, from.data(), from.size(), to.data(), to.size(), NULL, NULL))
+        if (!WideCharToMultiByte(toCP, dwFlags, from.data(), from.size(), to.data(), to.size(), NULL, NULL))
             { to.resize(0); return; }
-        to.resize(size-1);
     }
 
     template<int flags = MB_USEGLYPHCHARS>
     inline void ConvertCodePage(unsigned int fromCP, const std::string_view& from, std::wstring& to) {
-        size_t size = MultiByteToWideChar(fromCP, flags, from.data(), from.size(), NULL, NULL);
+        const DWORD dwFlags = fromCP == CP_UTF8 ? flags & WC_ERR_INVALID_CHARS : flags;
+        size_t size = MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), NULL, NULL);
         to.resize(size);
-        if (!MultiByteToWideChar(fromCP, flags, from.data(), from.size(), to.data(), to.size()))
+        if (!MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), to.data(), to.size()))
             { to.resize(0); return; }
-        to.resize(size-1);
     }
 }
+
+#endif
